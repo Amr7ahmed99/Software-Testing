a92c2a56ca2c24f1bc726054d1fe8bf773ee85cf
Amr7ahmed99 <amr.omar@maxab.io>
2023-07-14 19:50:28 +0300

    reorganize test file.

--- utils.spec.js
+++ utils.spec.js
@@ -1,24 +1,30 @@
 const { sum, greeting } = require("./utils");
 
 describe('UNIT_TESTING', ()=>{
-    test('sum - should be return 2+4= 6', ()=>{
-        const resultInInt= sum(2, 4)
-        expect(resultInInt).toBe(6)
-        expect(resultInInt).toBeGreaterThan(5)
-        expect(resultInInt).toBeGreaterThanOrEqual(6)
-
-    })
-
-    test('sum - should be return 0.102+0.3= 0.402', ()=>{
-        let resultInFloat= sum(0.102, 0.3)
-        expect(resultInFloat).toBeCloseTo(0.4)
-    })
-
-    test('greeting - should be return Hello Amr', ()=>{
-        const result= greeting("Amr");
-        expect(result).toMatch("Hello Amr");
-        expect(result).toMatch(/Hello Amr/);
-        // expect(result).toMatch(/^Hello Amr$/); //this case will be failed
-
-    })
+    describe('sum', ()=>{
+        it('should return 2+4= 6', ()=>{
+            const resultInInt= sum(2, 4)
+            expect(resultInInt).toBe(6)
+            expect(resultInInt).toBeGreaterThan(5)
+            expect(resultInInt).toBeGreaterThanOrEqual(6)
+    
+        })
+    
+        it('should return 0.102+0.3= 0.402', ()=>{
+            let resultInFloat= sum(0.102, 0.3)
+            expect(resultInFloat).toBeCloseTo(0.4)
+        })
+    });
+    
+    describe('greeting', ()=>{
+        it('should return Hello Amr', ()=>{
+            const result= greeting("Amr");
+            expect(result).toMatch("Hello Amr");
+            expect(result).toMatch(/Hello Amr/);
+            // expect(result).toMatch(/^Hello Amr$/); //this case will be failed
+    
+        })
+    });
+    
 });
+
